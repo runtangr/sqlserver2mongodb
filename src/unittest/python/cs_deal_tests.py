6ac7a1c397bf0db68f88fbe6e4305c6ff05f8484
tangrui <1048393815@qq.com>
2017-05-26 18:26:17 +0800

    成交明细测试

--- src/unittest/python/cs_deal_tests.py
+++ src/unittest/python/cs_deal_tests.py
@@ -6,6 +6,8 @@ Modified on May 25, 2017
 @author: tangr
 '''
 
+#成交明细
+
 import sys,os
 import unittest
 from suds.client import Client
@@ -20,14 +22,9 @@ from core.Utils import init_leancloud_client
 init_leancloud_client()
 class Test(unittest.TestCase):
 
-    def test_query(self):
-
+    def test_deal(self):
+        #测试接口
         url = "http://10.30.0.122:8091/Stocks.asmx?WSDL"
-        data = {
-                "Coordinates":"021525374658617185",
-                "Encryptionchar":"F5AC95F60BBEDAA9372AE29B84F5E67A",
-                "Userid":"24124"
-                 }
 
         client = Client(url)
         # print (client)
@@ -37,12 +34,22 @@ class Test(unittest.TestCase):
         data= json.loads(response)
         print (data)
 
-        Todo = leancloud.Object.extend('Todo')
-        todo = Todo()
-        todo.set('title', '工程师周会')
-        todo.set('content', '每周工程师会议，周一下午2点')
-        todo.set('location', '会议室')  # 增加一个字段
-        todo.save()
+        AnalogOrder = leancloud.Object.extend('AnalogOrder')
+        orderObj = AnalogOrder()
+
+        orderObj.set('stockCode', data["StockCode"])
+        orderObj.set('stockName', data["stockname"])
+        orderObj.set('marketCode', data["marketcode"])
+        orderObj.set('price', data["Price"])
+        orderObj.set('volume', data["Volume"])
+        orderObj.set('cjje', data["cjje"])
+        orderObj.set('transType', data["transType"])
+        orderObj.set('dealTime', data["dateTime"])
+        orderObj.set('profitorLoss', data["ProfitorLoss"])
+        orderObj.set('syl', data["syl"])
+
+        orderObj.save();
+
 
 if __name__ == "__main__":
 
