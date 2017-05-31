8dcda929112446be28208e44d5b859c80edb2ab2
tangrui <1048393815@qq.com>
2017-05-31 17:53:10 +0800

    添加财急送描述job.yaml

---
+++ src/main/python/Application/Daemon/Order.py
@@ -0,0 +1,21 @@
+#encoding=utf-8
+
+ #模拟炒股
+ #更新成交明细数据
+
+class Order:
+	def OrderPort(self):
+		pass
+
+	def OrderMC(self):
+		pass
+
+
+
+if __name__ == "__main__":
+
+	Order_object = Order()
+
+	Order_object.OrderPort()
+	Order_object.OrderMC()
+
