4e0f51b2515d6d71857a36eae72725b74b9ec0b9
tangrui <1048393815@qq.com>
2017-06-08 15:52:15 +0800

    添加专家点评tests

---
+++ src/unittest/python/CommentMC_tests.py
@@ -0,0 +1,42 @@
+#encoding=utf-8
+
+'''
+Modified on May 25, 2017
+
+@author: tangr
+'''
+
+#专家点评 MC存储测试模块
+
+import unittest
+from suds.client import Client
+import json
+from core import leancloud_patch
+import leancloud
+from core.Utils import init_leancloud_client
+
+init_leancloud_client()
+
+class TestComment(unittest.TestCase):
+
+    #写入mc数据测试
+    def test_mc(self):
+
+        #mc更新AnalogComment(专家点评)表
+        uimsZJDP = leancloud.Object.extend('uimsZJDP')
+        uimsZJDPObj = uimsZJDP()
+
+        uimsZJDPObj.set('rsOperateID', '1')
+
+        uimsZJDPObj.set('rsStatus', "1")
+        uimsZJDPObj.set('rsProjectId', "1")
+        uimsZJDPObj.set('groupbm', "qwert")
+        uimsZJDPObj.set('JudgeContent', "adsgasdgsbfhgs")
+
+        # 还有一些未写出来
+        uimsZJDPObj.save()
+
+
+if __name__ == "__main__":
+
+    unittest.main()
---
+++ src/unittest/python/CommentPort_tests.py
@@ -0,0 +1,44 @@
+#encoding=utf-8
+
+'''
+Modified on June 8, 2017
+
+@author: tangr
+'''
+
+#专家点评WebService 接口测试模块
+
+import unittest
+from suds.client import Client
+import json
+from core import leancloud_patch
+import leancloud
+from core.Utils import init_leancloud_client
+
+init_leancloud_client()
+
+class TestComment(unittest.TestCase):
+
+    #WebService 接口测试
+    def test_WebService(self):
+        print ("start!")
+
+        #测试地址
+        url = "http://180.169.122.18:8091/Stocks.asmx?WSDL"
+        client = Client(url)
+        # print (client)
+
+
+        #专家点评 WebService 测试接口Query_uimsZJDP
+        response = client.service.Query_uimsZJDP(Coordinates='021525374658617185',
+                                                Encryptionchar='F5AC95F60BBEDAA9372AE29B84F5E67A',
+                                                 rsMainkeyid=1,
+                                                 rsDatetime="2004-02-02"
+                                                                )
+        self.data= json.loads(response)
+        print (self.data)
+
+
+if __name__ == "__main__":
+
+    unittest.main()
