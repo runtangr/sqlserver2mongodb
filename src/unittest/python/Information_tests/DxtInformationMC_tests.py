cd157cf94774897c38e86e2968b18254c6595c53
tangrui <1048393815@qq.com>
2017-06-14 18:14:28 +0800

    添加 资讯tests

---
+++ src/unittest/python/Information_tests/DxtInformationMC_tests.py
@@ -0,0 +1,51 @@
+#encoding=utf-8
+
+'''
+Modified on June 12, 2017
+
+@author: tangr
+'''
+
+#自主新闻查询 MC存储测试模块
+
+import unittest
+from suds.client import Client
+import json
+import leancloud_patch
+import leancloud
+from Utils import init_leancloud_client
+
+init_leancloud_client()
+
+class TestCommNews(unittest.TestCase):
+
+    #写入mc数据测试
+    def test_mc(self):
+
+        #mc更新A_DxtInformation 资讯栏目更新表
+        A_DxtInformation = leancloud.Object.extend('A_DxtInformation')
+        A_DxtInformationObj = A_DxtInformation()
+
+        A_DxtInformationObj.set('title', "qwer")
+        A_DxtInformationObj.set('source', "")
+        A_DxtInformationObj.set('summary', "")  ##content
+        A_DxtInformationObj.set('thumbnail', "")
+        A_DxtInformationObj.set('url', "www.baidu.com")
+        A_DxtInformationObj.set('content', "afsafsa")
+        A_DxtInformationObj.set('srcContent', "afsafsa")
+
+        A_DxtInformationObj.set('author',"tangr")
+        A_DxtInformationObj.set('publishTime', "2017-06-14")
+        A_DxtInformationObj.set('clickNumber', "")
+        A_DxtInformationObj.set('likeNumber', "")
+        A_DxtInformationObj.set('shareNumber', "")
+        A_DxtInformationObj.set('collectNumber', "")
+        A_DxtInformationObj.set('relationId', "100")
+
+        # 还有一些未写出来
+        A_DxtInformationObj.save()
+
+
+if __name__ == "__main__":
+
+    unittest.main()
---
+++ src/unittest/python/Information_tests/DxtInformationRemarkPort_tests.py
@@ -0,0 +1,46 @@
+#encoding=utf-8
+
+'''
+Modified on June 12, 2017
+
+@author: tangr
+'''
+
+#钱坤晨会 WebService 接口测试模块
+
+import unittest
+from suds.client import Client
+import json
+import leancloud_patch
+import leancloud
+from Utils import init_leancloud_client
+
+init_leancloud_client()
+
+class TestCommNews(unittest.TestCase):
+
+    #WebService 接口测试
+    def test_WebService(self):
+        print ("start!")
+
+        #测试地址
+        url = "http://10.30.0.122:8093/Stocks.asmx?WSDL"
+        client = Client(url)
+        # print (client)
+
+
+        #钱坤晨会 WebService 测试接口Query_sirsReportRemark
+        response = client.service.Query_sirsReportRemark(Coordinates='021525374658617185',
+                                                Encryptionchar='F5AC95F60BBEDAA9372AE29B84F5E67A',
+                                                 rsMainkeyid=0,
+                                                 rsDatetime="2000-06-02 10:15:0",
+                                                 top=1
+                                                                )
+
+        self.data= json.loads(response)
+        print (self.data)
+
+
+if __name__ == "__main__":
+
+    unittest.main()
