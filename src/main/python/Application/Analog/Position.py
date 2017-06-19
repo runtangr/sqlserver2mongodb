c07628121e895deffd4e2df9c7e86d76ee7806b3
tangrui <1048393815@qq.com>
2017-06-19 10:57:53 +0800

    position 正式环境

--- src/main/python/Application/Analog/Position.py
+++ src/main/python/Application/Analog/Position.py
@@ -30,7 +30,7 @@ class Order:
 		'''
 			获取端口数据
 			'''
-		url = "http://10.30.0.122:8091/Stocks.asmx?WSDL"
+		url = "http://114.80.94.175:8084/Stocks.asmx?WSDL"
 		client = Client(url)
 		# print (client)
 
@@ -92,96 +92,105 @@ class Order:
 				print ("maxKeyId:", maxKeyId, "===", "rsMainkeyID:", DataObjArr['rsMainkeyID'], "===",
 					   "VGroupid:", DataObjArr['VGroupid'], "===", "rsDateTime:", DataObjArr['rsDateTime'], "\r\n")
 
-				try:
-					queryMyMatch = leancloud.Query('AnalogMyMatch')
-					queryMyMatch.equal_to('groupBmId', DataObjArr['VGroupid'])
-					myMatchObj = queryMyMatch.find()
-					if myMatchObj:
-						syl = '%.2f' %(DataObjArr['syl_all'] * 100)
-						sylday = '%.2f' %(DataObjArr['syl_day'] * 100)
-						sylweek = '%.2f' %(DataObjArr['syl_week'] * 100)
-						myMatchObj[0].set('originalCapital', DataObjArr['OriginalCapital']) #ok
-						myMatchObj[0].set('residualCapital', DataObjArr['ResidualCapital'])
-						myMatchObj[0].set('frozenCapital', DataObjArr['FrozenCapital'])
-						myMatchObj[0].set('syl', syl+'%')
-						myMatchObj[0].set('sylDay', sylday+"%")
-						myMatchObj[0].set('sylWeek', sylweek+"%")
-						myMatchObj[0].set('shouYiLv', round(DataObjArr['syl_all'] * 100, 2))#
-						myMatchObj[0].set('shouYiLvDay', round(DataObjArr['syl_day'] * 100, 2))
-						myMatchObj[0].set('shouYiLvWeek', round(DataObjArr['syl_week'] * 100, 2))
-						myMatchObj[0].set('pm', DataObjArr['pm_all'])
-						myMatchObj[0].set('pmDay', DataObjArr['pm_Day'])
-						myMatchObj[0].set('pmWeek', DataObjArr['pm_week'])
-						myMatchObj[0].set('tradeTotal', DataObjArr['tradeTotal'])
-						myMatchObj[0].set('tradeCountYL', DataObjArr['tradeCount'])
-						myMatchObj[0].set('myPopularity', DataObjArr['djs'])
-						myMatchObj[0].set('totalProfitorLoss', DataObjArr['sy_all'])
-						myMatchObj[0].set('userName', DataObjArr['ZhName'])
-						myMatchObj[0].save()
 
-					else:
-						#补齐用户
-						# queryUser = leancloud.Query('_User')
-                        #
-						# queryUser.equal_to('userId',DataObjArr['UserId'])
-						# try:
-						# 	userObj = queryUser.first()
-						# except Exception, e:
-						# 	userObj = leancloud.User()
-						# 	userObj.set_username(DataObjArr['CountName'])
-						# 	userObj.set_password('a123456')
-						# 	userObj.set('userId', DataObjArr['UserId'])
-						# 	userObj.set('nickname', DataObjArr['ZhName'])
-						# 	userObj.sign_up()
+				queryMyMatch = leancloud.Query('AnalogMyMatch')
+				queryMyMatch.equal_to('groupBmId', DataObjArr['VGroupid'])
+				myMatchObj = queryMyMatch.find()
+				if myMatchObj:
+					syl = '%.2f' %(DataObjArr['syl_all'] * 100)
+					sylday = '%.2f' %(DataObjArr['syl_day'] * 100)
+					sylweek = '%.2f' %(DataObjArr['syl_week'] * 100)
+					myMatchObj[0].set('originalCapital', DataObjArr['OriginalCapital']) #ok
+					myMatchObj[0].set('residualCapital', DataObjArr['ResidualCapital'])
+					myMatchObj[0].set('frozenCapital', DataObjArr['FrozenCapital'])
+					myMatchObj[0].set('syl', syl+'%')
+					myMatchObj[0].set('sylDay', sylday+"%")
+					myMatchObj[0].set('sylWeek', sylweek+"%")
+					myMatchObj[0].set('shouYiLv', round(DataObjArr['syl_all'] * 100, 2))#
+					myMatchObj[0].set('shouYiLvDay', round(DataObjArr['syl_day'] * 100, 2))
+					myMatchObj[0].set('shouYiLvWeek', round(DataObjArr['syl_week'] * 100, 2))
+					myMatchObj[0].set('pm', DataObjArr['pm_all'])
+					myMatchObj[0].set('pmDay', DataObjArr['pm_Day'])
+					myMatchObj[0].set('pmWeek', DataObjArr['pm_week'])
+					myMatchObj[0].set('tradeTotal', DataObjArr['tradeTotal'])
+					myMatchObj[0].set('tradeCountYL', DataObjArr['tradeCount'])
+					myMatchObj[0].set('myPopularity', DataObjArr['djs'])
+					myMatchObj[0].set('totalProfitorLoss', DataObjArr['sy_all'])
+					myMatchObj[0].set('userName', DataObjArr['ZhName'])
+					myMatchObj[0].save()
 
-						#大赛记录
-						queryMatch = leancloud.Query('AnalogMatch')
-						matchObject = queryMatch.first()
+				else:
+					#补齐用户
+					queryUser = leancloud.Query('_User')
 
-						syl = '%.2f'%(DataObjArr['syl_all'] * 100)
-						sylday = '%.2f'%(DataObjArr['syl_day'] * 100)
-						sylweek = '%.2f'%(DataObjArr['syl_week'] * 100)
-
-						AnalogMyMatch = leancloud.Object.extend('AnalogMyMatch')
-						MyMatchObj = AnalogMyMatch()
-
-						User = leancloud.Object.extend('_User')
-						queryUser =leancloud.Query(User)
-						queryUser.equal_to('userId', DataObjArr['UserId'])
+					queryUser.equal_to('userId',DataObjArr['UserId'])
+					try:
 						userObj = queryUser.first()
+					except Exception, e:
+						userObj = leancloud.User()
+						userObj.set_username(DataObjArr['CountName'])
+						userObj.set_password('a123456')
+						userObj.set('userId', DataObjArr['UserId'])
+						userObj.set('nickname', DataObjArr['ZhName'])
+						userObj.sign_up()
 
-						MyMatchObj.set('userObjectId',userObj.id)
-						MyMatchObj.set('headImageUrl',userObj.get('headImageUrl'))
-						MyMatchObj.set('userName', DataObjArr['ZhName'])
-						MyMatchObj.set('analogUserId', DataObjArr['UserId'])
-						MyMatchObj.set('matchObjectId',matchObject.id)
-						MyMatchObj.set('matchName',matchObject.get('matchName'))
-						MyMatchObj.set('analogMatchId',matchObject.get('analogMatchId'))
-						MyMatchObj.set('beginTime',matchObject.get('beginTime'))
-						MyMatchObj.set('endTime',matchObject.get('endTime'))
-						MyMatchObj.set('groupBmId', DataObjArr['VGroupid'])
-						MyMatchObj.set('originalCapital', DataObjArr['OriginalCapital'])
-						MyMatchObj.set('residualCapital', DataObjArr['ResidualCapital'])
-						MyMatchObj.set('frozenCapital', DataObjArr['FrozenCapital'])
-						MyMatchObj.set('syl', syl+"%")
-						MyMatchObj.set('sylDay', sylday+"%")
-						MyMatchObj.set('sylWeek', sylweek+"%")
-						MyMatchObj.set('shouYiLv', round(DataObjArr['syl_all'] * 100, 2))
-						MyMatchObj.set('shouYiLvDay', round(DataObjArr['syl_day'] * 100, 2))
-						MyMatchObj.set('shouYiLvWeek', round(DataObjArr['syl_week'] * 100, 2))
-						MyMatchObj.set('pm', DataObjArr['pm_all'])
-						MyMatchObj.set('pmDay', DataObjArr['pm_Day'])
-						MyMatchObj.set('pmWeek', DataObjArr['pm_week'])
-						MyMatchObj.set('tradeTotal', DataObjArr['tradeTotal'])
-						MyMatchObj.set('tradeCountYL', DataObjArr['tradeCount'])
-						MyMatchObj.set('myPopularity', DataObjArr['djs'])
-						MyMatchObj.set('totalProfitorLoss', DataObjArr['sy_all'])
-						MyMatchObj.set('isDefault', '0')
-						MyMatchObj.save()
-						myMatchObj[0] = MyMatchObj
+					#大赛记录
+					queryMatch = leancloud.Query('AnalogMatch')
+					matchObject = queryMatch.first()
 
-						#如果stockcode不等于000000则更新持仓数据
-					if DataObjArr['StockCode'] != "000000":
+					syl = '%.2f'%(DataObjArr['syl_all'] * 100)
+					sylday = '%.2f'%(DataObjArr['syl_day'] * 100)
+					sylweek = '%.2f'%(DataObjArr['syl_week'] * 100)
+
+					AnalogMyMatch = leancloud.Object.extend('AnalogMyMatch')
+					myMatchObject = AnalogMyMatch()
+
+					# User = leancloud.Object.extend('_User')
+					# queryUser =leancloud.Query(User)
+					# queryUser.equal_to('userId', DataObjArr['UserId'])
+					# queryUser.equal_to('userId', 2504655)
+					# queryUser.equal_to('userId', 96553)
+					# queryUser.limit(10)
+					# planList = queryUser.find()
+					# for plan in planList:
+					# 	print plan.dump()
+					# print len(planList)
+
+					# userObj = queryUser.first()
+
+					myMatchObject.set('userObjectId',userObj.id)
+					myMatchObject.set('headImageUrl',userObj.get('headImageUrl'))
+					myMatchObject.set('userName', DataObjArr['ZhName'])
+					myMatchObject.set('analogUserId', DataObjArr['UserId'])
+					myMatchObject.set('matchObjectId',matchObject.id)
+					myMatchObject.set('matchName',matchObject.get('matchName'))
+					myMatchObject.set('analogMatchId',matchObject.get('analogMatchId'))
+					myMatchObject.set('beginTime',matchObject.get('beginTime'))
+					myMatchObject.set('endTime',matchObject.get('endTime'))
+					myMatchObject.set('groupBmId', DataObjArr['VGroupid'])
+					myMatchObject.set('originalCapital', DataObjArr['OriginalCapital'])
+					myMatchObject.set('residualCapital', DataObjArr['ResidualCapital'])
+					myMatchObject.set('frozenCapital', DataObjArr['FrozenCapital'])
+					myMatchObject.set('syl', syl+"%")
+					myMatchObject.set('sylDay', sylday+"%")
+					myMatchObject.set('sylWeek', sylweek+"%")
+					myMatchObject.set('shouYiLv', round(DataObjArr['syl_all'] * 100, 2))
+					myMatchObject.set('shouYiLvDay', round(DataObjArr['syl_day'] * 100, 2))
+					myMatchObject.set('shouYiLvWeek', round(DataObjArr['syl_week'] * 100, 2))
+					myMatchObject.set('pm', DataObjArr['pm_all'])
+					myMatchObject.set('pmDay', DataObjArr['pm_Day'])
+					myMatchObject.set('pmWeek', DataObjArr['pm_week'])
+					myMatchObject.set('tradeTotal', DataObjArr['tradeTotal'])
+					myMatchObject.set('tradeCountYL', DataObjArr['tradeCount'])
+					myMatchObject.set('myPopularity', DataObjArr['djs'])
+					myMatchObject.set('totalProfitorLoss', DataObjArr['sy_all'])
+					myMatchObject.set('isDefault', '0')
+					myMatchObject.save()
+					myMatchObj.append(myMatchObject)
+					# myMatchObj[0] = myMatchObject
+
+					#如果stockcode不等于000000则更新持仓数据
+				if DataObjArr['StockCode'] != "000000":
 						queryStock = leancloud.Query('AnalogStock')
 						queryStock.equal_to('groupBmId', DataObjArr['VGroupid'])
 						queryStock.equal_to('marketCode', DataObjArr['marketcode'].strip())
@@ -246,8 +255,8 @@ class Order:
 							stockObj.set('marketName', DataObjArr['marketName'])
 							stockObj.set('buyMoney', DataObjArr['BuyMonney'])
 							stockObj.save()
-				except Exception,e:
-					logging.error("账户持仓数据更新失败: %s"%DataObjArr)
+				# except Exception,e:
+				# 	logging.error("账户持仓数据更新失败: %s"%DataObjArr)
 
 			if isChange == 1:
 				syncObj.set('mainKeyId', maxKeyId)
