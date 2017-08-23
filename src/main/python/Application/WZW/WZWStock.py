a1a02ddb2c5c2e4f9cf937471e21316c38251a3e
zhengxh <zxh@zqf.com.cn>
2017-08-23 15:39:15 +0800

    修改王中王持仓同步， 1. 持仓数为0时不进行行情请求 2.如果原持仓数据及即将更新的持仓数据都为0,则不进行保存处理

--- src/main/python/Application/WZW/WZWStock.py
+++ src/main/python/Application/WZW/WZWStock.py
@@ -1,4 +1,4 @@
-#encoding=utf-8
+# encoding=utf-8
 
 '''
 Modified on June 27, 2017
@@ -6,7 +6,7 @@ Modified on June 27, 2017
 @author: tangr
 '''
 
- #王中王持仓列表
+# 王中王持仓列表
 import os
 import unittest
 from suds.client import Client
@@ -19,12 +19,11 @@ from Utils import init_leancloud_client
 import logging
 import MarketData
 
-
 logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s.%(funcName)s Line:%(lineno)d %(message)s',
                     level=os.getenv("LOG_LEVEL", 'INFO'))
 
-
 init_leancloud_client()
+
 
 class WZWStock:
     def WZWStockPort(self):
@@ -41,11 +40,11 @@ class WZWStock:
 
         querySyncInfo.equal_to('type', 'WZWStock')
         syncObj = querySyncInfo.find()
-        if len(syncObj)== 0:
+        if len(syncObj) == 0:
             dataTime = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.localtime())
-            self.SyncControlObj.set("type","WZWStock")
-            self.SyncControlObj.set("mainKeyId",0)
-            self.SyncControlObj.set("rsDateTime","1990-01-01")
+            self.SyncControlObj.set("type", "WZWStock")
+            self.SyncControlObj.set("mainKeyId", 0)
+            self.SyncControlObj.set("rsDateTime", "1990-01-01")
             self.SyncControlObj.save()
 
         self.SyncControlObj = querySyncInfo.first()
@@ -55,16 +54,16 @@ class WZWStock:
 
         # 王中王持仓列表 WebService 测试接口P_Z_uimsVSTSC_pro
         response = client.service.P_Z_uimsVSTSC_pro(Coordinates='021525374658617185',
-                                                Encryptionchar='F5AC95F60BBEDAA9372AE29B84F5E67A',
-                                                rsMainkeyID=self.maxKeyId,
-                                                rsDateTime="2017-08-01 00:00:00",
-                                                  top=top
+                                                    Encryptionchar='F5AC95F60BBEDAA9372AE29B84F5E67A',
+                                                    rsMainkeyID=self.maxKeyId,
+                                                    rsDateTime="2017-08-01 00:00:00",
+                                                    top=top
                                                     )
         try:
             self.WZWStock = json.loads(response)
 
         except Exception, e:
-            logging.error("%s  webservice 接口数据获取失败 %s" %(__file__ ,response))
+            logging.error("%s  webservice 接口数据获取失败 %s" % (__file__, response))
 
     def WZWStockMC(self):
         '''
@@ -75,42 +74,44 @@ class WZWStock:
 
         if self.WZWStock["Code"] == 0:
 
-            self.DataObj =  json.loads(self.WZWStock["DataObj"])#
+            self.DataObj = json.loads(self.WZWStock["DataObj"])  #
 
-
-            map(self.DealData,self.DataObj)
+            map(self.DealData, self.DataObj)
 
 
         else:
-             logging.warning("提交王中王持仓列表数据返回失败：%s" %self.WZWStock)
+            logging.warning("提交王中王持仓列表数据返回失败：%s" % self.WZWStock)
 
-    def DealData(self,DataObjArr):
+    def DealData(self, DataObjArr):
         # logging.warning("1")
-        #最后一条数据赋值
-        if DataObjArr==self.DataObj[-1]:
+        # 最后一条数据赋值
+        if DataObjArr == self.DataObj[-1]:
             self.maxKeyId = int(DataObjArr['rsmainkeyid'])
             self.rsDateTime = DataObjArr['rsDateTime']
             self.SyncControlObj.set('mainKeyId', self.maxKeyId)
             self.SyncControlObj.set('rsDateTime', self.rsDateTime)
             self.SyncControlObj.save()
 
-        #打印
+        # 打印
         print ("maxKeyId:", self.maxKeyId, "===", "rsMainkeyID:", DataObjArr['rsmainkeyid'], "===",
                "rsDateTime:", DataObjArr['rsDateTime'])
 
-        #数据处理
-        #时间格式
-        self.firstBuyDate = datetime.strptime(DataObjArr['FirstBuyDate'],'%Y-%m-%d %H:%M:%S')
+        # 数据处理
+        # 时间格式
+        self.firstBuyDate = datetime.strptime(DataObjArr['FirstBuyDate'], '%Y-%m-%d %H:%M:%S')
         # totalCapital = DataObjArr["ResidualCapital"] + DataObjArr["ResidualCapital"]
         # logging.warning("2")
-        #持仓市价  持仓市值 = 行情接口获取当前价 * 当前持仓股数
-        market_data =MarketData.getTicker(DataObjArr["marketcode"] + DataObjArr['stockcode'])
-        if market_data.ZuiXinJia:
-            current = market_data.ZuiXinJia/10000.00
+        if DataObjArr["currentVolume"] > 0:
+            # 持仓市价  持仓市值 = 行情接口获取当前价 * 当前持仓股数
+            market_data = MarketData.getTicker(DataObjArr["marketcode"] + DataObjArr['stockcode'])
+            if market_data.ZuiXinJia:
+                current = market_data.ZuiXinJia / 10000.00
+            else:
+                current = market_data.ZuoShou / 10000.00
         else:
-            current = market_data.ZuoShou/10000.00
+            current = 0
         # logging.warning("3")
-        self.position_sz =current * DataObjArr["currentVolume"]
+        self.position_sz = current * DataObjArr["currentVolume"]
 
         A_DxtWZWStockStockQuery = leancloud.Query('A_DxtWZWStock')
         A_DxtWZWStockStockQuery.equal_to('relationId', DataObjArr['rsmainkeyid'])
@@ -122,16 +123,20 @@ class WZWStock:
         self.A_DxtWZWTeacherList = A_DxtWZWTeacherQuery.find()
         # 编辑  王中王老师有 才会有持仓
         # logging.warning("4")
-        if len(self.A_DxtWZWStockStockList) > 0 and len(self.A_DxtWZWTeacherList)>0:
+        if len(self.A_DxtWZWStockStockList) > 0 and len(self.A_DxtWZWTeacherList) > 0:
 
-            self.Save(self.A_DxtWZWStockStockList[0],DataObjArr)
-        elif len(self.A_DxtWZWTeacherList)>0:
+            self.Save(self.A_DxtWZWStockStockList[0], DataObjArr)
+        elif len(self.A_DxtWZWTeacherList) > 0:
             A_DxtWZWStock = leancloud.Object.extend('A_DxtWZWStock')
             A_DxtWZWStockObj = A_DxtWZWStock()
-            self.Save(A_DxtWZWStockObj,DataObjArr)
+            self.Save(A_DxtWZWStockObj, DataObjArr)
 
-        # logging.warning("5")
-    def Save(self,Obj,DataObjArr):
+            # logging.warning("5")
+
+    def Save(self, Obj, DataObjArr):
+        # 如果原持仓数据及即将更新的持仓数据都为0,则不进行处理
+        if Obj.get('currentVolume', 0) and DataObjArr["currentVolume"]:
+            return
         Obj.set('groupBmId', DataObjArr['VGroupid'])
         Obj.set('teacherObjectId', self.A_DxtWZWTeacherList[0].get("objectId"))
         Obj.set('stockCode', DataObjArr['stockcode'])
@@ -154,14 +159,13 @@ class WZWStock:
         # if (DataObjArr['rsMainkeyID'] == 34099):
         #     a=1
 
-        #持仓市价
+        # 持仓市价
         Obj.set('total_sz', self.position_sz)
         Obj.save()
 
+
 if __name__ == "__main__":
+    WZWStock_object = WZWStock()
 
-	WZWStock_object = WZWStock()
-
-	WZWStock_object.WZWStockPort()
-	WZWStock_object.WZWStockMC()
-
+    WZWStock_object.WZWStockPort()
+    WZWStock_object.WZWStockMC()
